4d942de6b42baefb2254cd483a267b2f8bfe1deb
stephentetley <stephen.tetley@gmail.com>
2024-03-04 22:48:45 +0000

    Added figure 5 from the Souffle RAM machine paper translated to Python with DuckDB.

---
+++ python/path_duckdb.py
@@ -0,0 +1,67 @@
+# Figure 5 from On Fast Large-Scale Program Analysis in Datalog
+# use conda::datafusion-env [Ctrl+Shift+P then `Python: Select Interpreter`]
+
+import duckdb
+
+
+duckdb_path = 'e:/coding/python/fixpoint-experiment/python/path.duckdb'
+
+con = duckdb.connect(database=duckdb_path, read_only=False)
+
+con.execute("CREATE OR REPLACE TABLE edge (edge_from INTEGER, edge_to INTEGER);")
+con.execute("CREATE OR REPLACE TABLE path (path_from INTEGER, path_to INTEGER, PRIMARY KEY(path_from, path_to));")
+con.execute("CREATE OR REPLACE TABLE delta_path (path_from INTEGER, path_to INTEGER);")
+con.execute("CREATE OR REPLACE TABLE new_path (path_from INTEGER, path_to INTEGER);")
+con.execute("CREATE OR REPLACE TABLE path_join (path_from INTEGER, path_to INTEGER);")
+
+
+con.execute("INSERT INTO edge (edge_from, edge_to) VALUES (1, 2), (2, 3);")
+print("edge")
+con.table("edge").show()
+
+# project_into
+con.execute("INSERT INTO path (path_from, path_to) SELECT edge_from, edge_to FROM edge;")
+print("path")
+con.table("path").show()
+
+# merge_into(path, delta_path) 
+con.execute("INSERT INTO delta_path (path_from, path_to) SELECT path_from, path_to FROM path;")
+print("delta_path")
+con.table("delta_path").show()
+
+
+# loop - use a vacuous condition, actual condition tested for `break`
+one_hundred = 100
+while one_hundred > 0:
+    # purge
+    con.execute("DELETE FROM new_path;")
+
+
+    con.execute("DELETE FROM path_join;")
+    con.execute("INSERT INTO path_join (path_from, path_to) SELECT l.edge_from AS path_from, r.path_to AS path_to FROM edge l JOIN delta_path r ON l.edge_to = r.path_from;")
+    con.execute("INSERT OR IGNORE INTO path (path_from, path_to) SELECT l.path_from, l.path_to FROM path_join l;")
+
+
+    sc = con.execute("SELECT COUNT(*) FROM new_path;").fetchone()
+    if sc is None:
+        count_tuples = 0
+    else:
+        count_tuples = sc[0]
+    print("count_tuples: {}".format(count_tuples))
+
+    if count_tuples == 0: 
+        break
+    
+    # merge_into(new_path, path)
+    con.execute("INSERT INTO path (path_from, path_to) SELECT path_from, path_to FROM new_path;")
+    
+    # swap(new_path, delta_path)
+    con.execute("ALTER TABLE new_path RENAME TO new_path1;")
+    con.execute("ALTER TABLE delta_path RENAME TO new_path;")
+    con.execute("ALTER TABLE new_path1 RENAME TO delta_path;")
+
+
+print("Done - path:")
+con.table("path").show()
+
+con.close()
