0049122e143b97950f3f20d4d5c3b7db64c02b8a
stephentetley <stephen.tetley@gmail.com>
2024-03-05 10:25:46 +0000

    Making path_duckdb.py more like the figure 5, though currently the insert in the loop is a big step.

--- python/path_duckdb.py
+++ python/path_duckdb.py
@@ -7,10 +7,26 @@ import duckdb
 duckdb_path = 'e:/coding/python/fixpoint-experiment/python/path.duckdb'
 
 
-def swap(t1: str, t2: str, *, con: duckdb.DuckDBPyConnection) -> None:
-    con.execute(f"ALTER TABLE {t1} RENAME TO {t1}_swap;")
-    con.execute(f"ALTER TABLE {t2} RENAME TO {t1};")
-    con.execute(f"ALTER TABLE {t1}_swap RENAME TO {t2};")
+def swap(table1: str, table2: str, *, con: duckdb.DuckDBPyConnection) -> None:
+    table_swap = f"{table1}_swap"
+    con.execute(f"ALTER TABLE {table1} RENAME TO {table_swap};")
+    con.execute(f"ALTER TABLE {table2} RENAME TO {table1};")
+    con.execute(f"ALTER TABLE {table_swap} RENAME TO {table2};")
+
+def purge(table: str, *, con: duckdb.DuckDBPyConnection) -> None:
+    con.execute(f"DELETE FROM {table};")
+
+def merge_into(table_from: str, table_into:str, columns: list[str], *, con: duckdb.DuckDBPyConnection) -> None:
+    cols = ", ".join(columns)
+    con.execute(f"INSERT OR IGNORE INTO {table_into} ({cols}) SELECT {cols} FROM {table_from};")
+
+def count_tuples(table: str, *, con: duckdb.DuckDBPyConnection) -> int:
+    ans1 = con.execute(f"SELECT COUNT(*) FROM {table};").fetchone()
+    if ans1 is None:
+        return 0
+    else:
+        return ans1[0]
+    
 
 
 con = duckdb.connect(database=duckdb_path, read_only=False)
@@ -19,8 +35,16 @@ con.execute("CREATE OR REPLACE TABLE edge (edge_from INTEGER, edge_to INTEGER);"
 con.execute("CREATE OR REPLACE TABLE path (path_from INTEGER, path_to INTEGER, PRIMARY KEY(path_from, path_to));")
 con.execute("CREATE OR REPLACE TABLE delta_path (path_from INTEGER, path_to INTEGER, PRIMARY KEY(path_from, path_to));")
 con.execute("CREATE OR REPLACE TABLE new_path (path_from INTEGER, path_to INTEGER, PRIMARY KEY(path_from, path_to));")
-con.execute("CREATE OR REPLACE TABLE path_join (path_from INTEGER, path_to INTEGER);")
 
+path1_sql = """
+    INSERT OR IGNORE INTO new_path (path_from, path_to) 
+    SELECT path_from, path_to FROM
+        (SELECT t1.edge_from as path_from, t2.path_to as path_to 
+        FROM edge t1
+        JOIN delta_path t2 ON t1.edge_to = t2.path_from)
+        EXCEPT
+        (SELECT t3.path_from, t3.path_to FROM new_path t3);
+"""
 
 con.execute("INSERT INTO edge (edge_from, edge_to) VALUES (1, 2), (2, 3), (3, 4);")
 print("edge")
@@ -31,38 +55,29 @@ con.execute("INSERT INTO path (path_from, path_to) SELECT edge_from, edge_to FRO
 print("path")
 con.table("path").show()
 
-# merge_into(path, delta_path) 
-con.execute("INSERT INTO delta_path (path_from, path_to) SELECT path_from, path_to FROM path;")
+
+merge_into("path", "delta_path", ["path_from", "path_to"], con=con)
+
 print("delta_path")
 con.table("delta_path").show()
 
 
-# loop - use a vacuous condition, actual condition tested for `break`
+# loop - use a vacuous condition, actual condition tested for before the `break` statement
 one_hundred = 100
 while one_hundred > 0:
     # purge
-    con.execute("DELETE FROM new_path;")
+    purge("new_path;", con=con)
 
 
-    con.execute("DELETE FROM path_join;")
-    con.execute("INSERT INTO path_join (path_from, path_to) SELECT l.edge_from AS path_from, r.path_to AS path_to FROM edge l JOIN delta_path r ON l.edge_to = r.path_from;")
-    con.execute("INSERT OR IGNORE INTO new_path (path_from, path_to) SELECT l.path_from, l.path_to FROM path_join l;")
+    con.execute(path1_sql)
 
 
-    sc = con.execute("SELECT COUNT(*) FROM new_path;").fetchone()
-    if sc is None:
-        count_tuples = 0
-    else:
-        count_tuples = sc[0]
-    print("count_tuples: {}".format(count_tuples))
-
-    if count_tuples == 0: 
+    tcount = count_tuples("new_path", con=con)
+    print(f"count_tuples: {tcount}")
+    if tcount == 0: 
         break
     
-    # merge_into(new_path, path)
-    con.execute("INSERT INTO path (path_from, path_to) SELECT path_from, path_to FROM new_path;")
-    
-    # swap(new_path, delta_path)
+    merge_into("new_path", "path", ["path_from", "path_to"], con=con)
     swap("new_path", "delta_path", con=con)
 
 
